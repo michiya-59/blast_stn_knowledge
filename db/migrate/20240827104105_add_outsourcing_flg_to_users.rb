b69aed203623634653ba9d7beb8b87f863a4235b
michiya-59 <nishino.michiya0509@gmail.com>
2024-10-11 00:50:05 +0900

    マイグレーションファイルの修正

--- db/migrate/20240827104105_add_outsourcing_flg_to_users.rb
+++ db/migrate/20240827104105_add_outsourcing_flg_to_users.rb
@@ -2,6 +2,8 @@
 
 class AddOutsourcingFlgToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :outsourcing_flg, :boolean, default: false, null: false
+    unless column_exists?(:users, :outsourcing_flg)
+      add_column :users, :outsourcing_flg, :boolean, default: false, null: false
+    end
   end
 end
